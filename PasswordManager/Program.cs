b671e79e050a3407e41e1cc8e9aaeff46e591950
jacdevos <jacdevos@gmail.com>
2016-10-02 17:04:32 +0200

    Create initial program to capture and check password

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,82 @@ namespace PasswordManager
     {
         static void Main(string[] args)
         {
+            AddOrChangePassword();
+            CheckPassword();
+            Console.ReadKey();
         }
+
+        public static void AddOrChangePassword()
+        {
+            //input user name
+            Console.WriteLine("Enter your user name");
+            string username = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                Console.WriteLine("User name cannot be empty. Try again.");
+                //try again
+                AddOrChangePassword();
+                return;
+            }
+
+            //input password
+            Console.WriteLine("Enter your new password:");
+            string password = Console.ReadLine();
+
+            //Validate password criteria
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                Console.WriteLine("Password cannot be empty. Try again.");
+                //try again
+                AddOrChangePassword();
+                return;
+            }
+            else if (password.Length < 6)
+            {
+                Console.WriteLine("Should be at least 6 chars. Try again.");
+                //try again
+                AddOrChangePassword();
+                return;
+            }
+
+            //Calculate hash
+            byte[] hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            //Save password
+            File.WriteAllBytes(username, hash);
+        }
+
+        public static void CheckPassword()
+        {
+
+            //input user name and password
+            Console.WriteLine("Enter your user name");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            //Calculate hash
+            byte[] hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(password));
+            byte[] savedHash = null;
+
+            try
+            {
+                savedHash = File.ReadAllBytes(username);
+            }
+            catch (Exception)
+            { /*gulp*/}
+
+            if (hash == null || savedHash == null || savedHash.SequenceEqual(hash))
+            {
+                Console.WriteLine("Correct password");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password");
+            }
+
+        }
+        
     }
 }
